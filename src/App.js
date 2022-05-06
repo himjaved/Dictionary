c19736e37e0794ca054f9fc723f9a084c8f42727
HImja Ved <himja1414@gmail.com>
2022-05-06 11:25:50 +1000

    removed bugs

--- src/App.js
+++ src/App.js
@@ -13,8 +13,21 @@ function App() {
         </header>
         <footer className="text-center">
           Coded by Himja Ved Open Sourced on{" "}
-          <a href="https://github.com/himjaved/Dictionary">Github</a> and hosted
-          on <a href="https://curious-pudding-1afc3c.netlify.app/">Netlify</a>
+          <a
+            href="https://github.com/himjaved/Dictionary"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://curious-pudding-1afc3c.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   if (props.phonetic) {
     return (
       <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
           Listen
         </a>
         <br />
